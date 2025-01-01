6beae67f5ece9fc00b7d7a242354f5b80db68729
sahaln <sahalnurdin@gmail.com>
2025-01-01 21:04:43 +0700

    finish: Beranda page

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -14,9 +14,8 @@
                     <div class="relative">
                         <div
                             class="flex items-center justify-center w-10 h-10 rounded-full bg-white  transition-all duration-300 hover:shadow-[0_10px_20px_0_#EBF40080]">
-                            <img src="/assets/images/icons/cart-hitam.svg" class="object-contain w-5 h-5" alt="icon">
+                            <img src="/assets/images/icons/cart-hitam.svg" class="object-contain" alt="icon">
                         </div>
-
                         @if ($cartItemCount > 0)
                             <span
                                 class="absolute bottom-0 right-0 flex items-center justify-center w-4 h-4 text-xs font-semibold text-white bg-[#EBF400] rounded-full">
@@ -40,9 +39,9 @@
             </button>
         </form>
     </div>
-    <section id="Categories" class="mt-[30px] ">
+    <section id="Categories" class="mt-[30px]  ">
         <div class="flex items-center justify-between px-5">
-            <h2 class="font-semibold">Kategori Menu</h2>
+            <h2 class="text-lg font-semibold md:text-xl">Kategori Menu</h2>
         </div>
         <div class="w-full px-5 mt-3">
             <div class="grid grid-cols-4 gap-2 justify-items-stretch sm:grid-cols-4 lg:grid-cols-4 ">
@@ -72,7 +71,7 @@
         <section id="Diskon" class="mt-[30px] ">
             <div class="flex items-center justify-between px-5 my-4">
                 <div class="flex gap-2 ">
-                    <h2 class="font-semibold">Diskon Hari ini</h2>
+                    <h2 class="text-lg font-semibold md:text-xl">Diskon Hari ini</h2>
                     <img src="/assets/images/icons/discount.svg" alt="discount">
                 </div>
                 <a href="{{ route('discount') }}" class="text-xs font-medium">Lihat Semua</a>
@@ -97,10 +96,10 @@
                                     </div>
                                     <div>
                                         {{-- NAMA PRODUK --}}
-                                        <h3 class="min-h-[14px] text-lg font-semibold leading-[27px] truncate">
+                                        <h3 class="min-h-[14px] text-sm md:text-lg font-semibold leading-[27px] truncate ">
                                             {{ $product->name }}</h3>
                                         {{-- HARGA --}}
-                                        <p class="mt-auto mb-8 text-lg font-semibold ">Rp
+                                        <p class="mt-auto mb-8 text-sm font-semibold md:text-lg ">Rp
                                             {{ number_format($product->total, 0, ',', '.') }} <span
                                                 class="text-sm font-normal text-red-500 line-through text-ngekos-gray">Rp
                                                 {{ number_format($product->price, 0, ',', '.') }}</span></p>
@@ -138,31 +137,28 @@
             @if ($category->products->isNotEmpty())
                 <section id="category-{{ $category->slug }}" class="mt-[30px] pb-[20px]">
                     <div class="flex items-center justify-between px-5 my-4">
-                        <h2 class="font-semibold">{{ $category->name }} Babon</h2>
+                        <h2 class="text-lg font-semibold md:text-xl">{{ $category->name }} Babon</h2>
                     </div>
-                    <div class="grid w-full grid-cols-1 gap-6 my-4 mt-3 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+                    <div class="grid w-full grid-cols-1 gap-6 my-4 mt-3 sm:grid-cols-2 md:grid-cols-3 ">
                         @foreach ($category->products as $product)
-                            <div class="px-5 last:pb-[40px]">
+                            <div class=" px-5 last:pb-[40px]">
                                 <a href="{{ route('front.detail', ['product' => $product->slug]) }}" class="card">
-                                    <div
-                                        class="flex flex-row justify-between w-full gap-2 pb-5 transition-all duration-300 rounded-xl">
-
+                                    <div class="flex flex-row justify-between w-full gap-2 pb-5 rounded-xl">
                                         <div class="w-6/12">
-                                            <h3 class="min-h-[14px] text-lg font-semibold leading-[27px] truncate">
+                                            <h3
+                                                class="min-h-[14px] md:text-lg text-sm font-semibold leading-[27px] truncate ">
                                                 {{ $product->name }}
                                             </h3>
-                                            <p class="mt-auto mb-8 text-lg font-semibold">
+                                            <p class="mt-auto mb-8 text-sm font-semibold md:text-lg">
                                                 Rp {{ number_format($product->price, 0, ',', '.') }}
                                             </p>
                                         </div>
-
                                         <div class="flex flex-col items-end justify-end w-auto">
                                             <!-- Image container -->
                                             <div class="w-full max-w-[240px] max-h-[150px]">
                                                 <img src="{{ Storage::url($product->thumbnail) }}" alt="image"
                                                     class="object-contain w-full max-w-[240px] h-auto max-h-[150px] rounded-xl" />
                                             </div>
-
                                             <!-- Button container -->
                                             <div class="flex justify-center w-full -mt-8">
                                                 <form
